e05bee0f9a7e939018161fb891b4093d97e6fa72
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-15 19:28:21 +0400

    add a separate Textarea component with all necessary files

---
+++ src/components/Inputs/Textarea/Textarea.styled.js
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+export const STextarea = styled.textarea`
+  width: 41.3125rem;
+  height: 8.6875rem;
+  border: .0625rem solid #2984CE;
+  border-radius: .625rem;
+  padding: 1.0625rem 1.25rem;
+  margin-top: 3.5rem;
+  &:focus{
+    outline: none;
+  }
+`
