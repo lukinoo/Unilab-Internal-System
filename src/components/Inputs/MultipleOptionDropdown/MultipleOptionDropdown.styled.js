edd20b73c2af52a74cdf6f99f4ca21ccac5632f7
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-18 17:13:04 +0400

    add custom margin to multipleOptionDropdown

--- src/components/Inputs/MultipleOptionDropdown/MultipleOptionDropdown.styled.js
+++ src/components/Inputs/MultipleOptionDropdown/MultipleOptionDropdown.styled.js
@@ -6,6 +6,7 @@ export const SMultipleDropdownList = styled(SDropdownList)`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-left: 30%;
 `
 
 export const SMultipleDropdownItem = styled(SDropdownItem)`
