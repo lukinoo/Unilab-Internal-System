fb0ead7f8aaf353b33a71585a3dd635de4f1efae
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-20 16:01:38 +0400

    change SNameSpan border to outline; add props logic to adjust margin of SNameSpan component; change padding rems

--- src/components/Inputs/MultipleOptionInput/MultipleOptionInput.styled.js
+++ src/components/Inputs/MultipleOptionInput/MultipleOptionInput.styled.js
@@ -3,19 +3,22 @@ import styled from 'styled-components';
 export const SMultipleOptionDiv = styled.div`
   display: flex;
   flex-wrap: wrap;
-  padding: .5rem 1.0938rem .5rem .5625rem;
+  padding: .5625rem 1.1rem .5rem .5625rem;
   width: ${({width})=>width};
   min-height: 2.55rem;
+ 
+  &>span{
+    margin: ${({itemCount})=>itemCount === 1 ? '' : '0 .5rem .2rem 0'};
+  }
 `
 export const SNameSpan = styled.span`
-  background-color: #D1D1D1;
+  background-color: #EFEFEF;
   color: #353535;
   display: inline-flex;
   align-items: center;
   justify-content: space-between;
   padding: .3125rem .6875rem .375rem .5rem;
-  border: .0625rem solid #D1D1D1;
-  margin: 0 .5rem .2rem 0;
+  outline: .0625rem solid #D1D1D1;
   border-radius: .25rem;
   font-size: .625rem;
 
