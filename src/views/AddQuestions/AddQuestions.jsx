38fa9997ec3be0087a4096c583a0287f19518b7e
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-25 18:33:38 +0400

    add state for question types

--- src/views/AddQuestions/AddQuestions.jsx
+++ src/views/AddQuestions/AddQuestions.jsx
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import { QuestionTypeDropdown } from "../../components/Inputs/QuestionTypeDropdown";
+
+const questionTypes = {
+  1: 'Checkbox',
+  2: 'Multiple Choice',
+  3: 'Textbox',
+  4: 'Rating Scale'
+}
+
 export const AddQuestions = () =>{
-  
+
+  const [questionTypeId, setQuestionTypeId] = useState(1);
+
+
   return (
     <>
       <h1>Add Questions Page</h1>
+      <QuestionTypeDropdown 
+        setQuestionTypeId={setQuestionTypeId}
+        value={questionTypeId}
+        items={questionTypes}
+      />
     </>
   );
 }
