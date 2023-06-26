f40f7d91af3b4ad39678f6587bc7740b768397c0
vazha2000 <vaja.siqtura2000@gmail.com>
2023-06-26 15:25:09 +0400

    display question input

--- src/views/AddQuestions/AddQuestions.jsx
+++ src/views/AddQuestions/AddQuestions.jsx
@@ -1,8 +1,11 @@
+import { QuestionInput } from "../../components/Inputs/QuestionInput";
+
 export const AddQuestions = () =>{
   
   return (
     <>
       <h1>Add Questions Page</h1>
+      <QuestionInput />
     </>
   );
 }
