f2f21d2459a6c4ae9bbbec5d6526aec1bce34449
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-28 00:36:00 +0400

    add first version of working dnd fuctional

--- src/components/AnswersContainer/AnswersContainer.jsx
+++ src/components/AnswersContainer/AnswersContainer.jsx
@@ -1,5 +1,51 @@
-export const AnswersContainer = () =>{
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "@hello-pangea/dnd";
+import { SAnswersContainer } from "./AnswersContainer.styled";
+
+
+export const AnswersContainer = () => {
+  const [answers, setAnswers] = useState([
+    { id: "a1", content: "Answer 1" },
+    { id: "a2", content: "Answer 2" },
+    { id: "a3", content: "Answer 3" },
+  ]);
+
+  const handleDragEnd = (result) => {
+    if (!result.destination) {
+      return; // no destination, do nothing
+    }
+
+    const newAnswers = [...answers];
+    const [reorderedItem] = newAnswers.splice(result.source.index, 1);
+    newAnswers.splice(result.destination.index, 0, reorderedItem);
+
+    setAnswers(newAnswers);
+  };
+
   return (
-    <></>
+    <DragDropContext onDragEnd={handleDragEnd}>
+      <SAnswersContainer>
+        <Droppable droppableId="answers">
+          {(provided) => (
+            <div {...provided.droppableProps} ref={provided.innerRef}>
+              {answers.map((item, index) => (
+                <Draggable key={item.id} draggableId={item.id} index={index}>
+                  {(provided) => (
+                    <div
+                      ref={provided.innerRef}
+                      {...provided.dragHandleProps}
+                      {...provided.draggableProps}
+                    >
+                      {item.content}
+                    </div>
+                  )}
+                </Draggable>
+              ))}
+              {provided.placeholder}
+            </div>
+          )}
+        </Droppable>
+      </SAnswersContainer>
+    </DragDropContext>
   );
-}
+};
