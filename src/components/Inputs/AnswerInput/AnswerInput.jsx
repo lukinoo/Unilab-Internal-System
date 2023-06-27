e46f441fa56ef548a281d31c0dee758015b61e51
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-28 01:34:52 +0400

    add starting code for answerInput component

--- src/components/Inputs/AnswerInput/AnswerInput.jsx
+++ src/components/Inputs/AnswerInput/AnswerInput.jsx
@@ -1,5 +1,33 @@
-export const AnswerInput = () => {
+import { SAnswerInputWrapper, SAnswerInput, SAnswerContainer } from "./AnswerInput.styled"
+
+export const AnswerInput = ({
+  width,
+  onClick,
+  onChange,
+  value, 
+  name,
+  fontSize,
+  fontWeight,
+  register,
+  LeftComponent,
+  RightComponent,
+  SelectOption,
+}) => {
   return (
-    <div></div>
+    <SAnswerContainer onClick={onClick}>
+      {LeftComponent}
+      {SelectOption}
+      <SAnswerInputWrapper>
+        <SAnswerInput
+          name={name}
+          width={width}
+          fontSize={fontSize}
+          fontWeight={fontWeight}
+          onChange={onChange}
+        />
+      </SAnswerInputWrapper>
+      {RightComponent}
+    </SAnswerContainer>
+
   )
 }
