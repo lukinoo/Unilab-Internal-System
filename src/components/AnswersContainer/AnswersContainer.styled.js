32f77e5810d3908d4c698d0c20e3e68578e79124
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-28 23:27:42 +0400

    add checkbox styles

--- src/components/AnswersContainer/AnswersContainer.styled.js
+++ src/components/AnswersContainer/AnswersContainer.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import checkmark from "/assets/svg/greenCheckmark.svg";
 
 export const SAnswersContainer = styled.div`
   height: fit-content;
@@ -6,4 +7,20 @@ export const SAnswersContainer = styled.div`
 
 export const SAnswerDiv = styled.div`
   margin-top: 1rem;
+`
+
+export const SCheckbox = styled.input`
+  all: unset;
+  width: 1.75rem;
+  height: 1.75rem;
+  aspect-ratio: 1;
+  border: .125rem solid #ffffff;
+  border-radius: .1875rem;
+  cursor: pointer;
+  &:checked{
+    background-color: #ffffff;
+    background-image: url(${checkmark});
+    background-position: center;
+    background-repeat: no-repeat;
+  }
 `
