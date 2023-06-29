ce8105b2fe366922cf525ae54ff0ecde6cc1f181
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-06-29 22:54:41 +0400

    add Button to add answers; Add function to append answers array

--- src/views/AddQuestions/AddQuestions.jsx
+++ src/views/AddQuestions/AddQuestions.jsx
@@ -2,6 +2,9 @@ import { QuestionInput } from "../../components/Inputs/QuestionInput";
 import { useState } from "react";
 import { QuestionTypeDropdown } from "../../components/Inputs/QuestionTypeDropdown";
 import { AnswersContainer } from "../../components/AnswersContainer";
+import { Button } from "../../components/Button/Button";
+import { SPlusIcon } from "./AddQuestions.styled";
+import plusSvg from "/assets/svg/plus.svg";
 
 const items = {
   1: "Checkbox",
@@ -19,7 +22,11 @@ export const AddQuestions = () =>{
     { id: "3", content: "" },
   ]);
   
-  
+  const addAnswer = () => {
+    const lastId = parseInt(answers[answers.length-1].id); // get last id
+    setAnswers((prevAnswers) => [...prevAnswers, { id: `${lastId+1}`, content: "" }]);
+  };
+
   return (
     <>
       <h1>Add Questions Page</h1>
@@ -34,6 +41,15 @@ export const AddQuestions = () =>{
         setAnswers={setAnswers}
         type={items[questionTypeId]}
       />
+      <Button
+        width="13.5rem"
+        height="3rem"
+        fontSize=".875rem"
+        LeftComponent={<SPlusIcon src={plusSvg} alt=""/>}
+        onClick={addAnswer}
+      >
+        პასუხის დამატება
+      </Button>
     </>
   );
 }
