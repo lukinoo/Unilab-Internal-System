754a82fdc067af4ca164b2e32e60f987ca9b8c32
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-03 01:06:05 +0400

    add textareaAnswer container

--- src/components/TextareaAnswer/TextareaAnswer.jsx
+++ src/components/TextareaAnswer/TextareaAnswer.jsx
@@ -1,5 +1,29 @@
-export const  TextareaAnswer = () => {
+import { Textarea } from "../Inputs/Textarea/Textarea";
+import { STextareaDiv } from "./TextareaAnswer.styled";
+import { SCross } from "../DraggableAnswersContainer/DraggableAnswersContainer.styled";
+import XSvg from "/assets/svg/xCross.svg";
+
+export const TextareaAnswer = ({
+  answers,
+  setAnswers,
+  deleteAnswer,
+  type,
+  control,
+}) => {
   return (
-    <div>TextareaAnswer</div>
-  )
-}
+    <div>
+      {answers.map((answerObj) => (
+        <STextareaDiv>
+          <Textarea
+            name={`textAnswer${answerObj.id}`}
+            control={control}
+            bgColor="transparent"
+            color="#ffffff"
+            resize={false}
+          />
+          <SCross src={XSvg} alt="" onClick={() => deleteAnswer(answerObj.id)} />
+        </STextareaDiv>
+      ))}
+    </div>
+  );
+};
