ba8467edde1e3c5da5bbf5be11d7c0074b1179bc
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-05 21:29:23 +0400

    remove comments; fix textarea border color

--- src/components/Inputs/Textarea/Textarea.styled.js
+++ src/components/Inputs/Textarea/Textarea.styled.js
@@ -1,11 +1,9 @@
 import styled from "styled-components"
 
 export const STextarea = styled.textarea`
-  /* width: 41.3125rem; */
   width: ${({width})=>width ? width : '41.3125rem'};
   height: ${({height})=>height ? height : '8.6875rem'};
-  /* height: 8.6875rem; */
-  border: .0625rem solid #2984CE;
+  border: .0625rem solid #ffffff;
   border-radius: .625rem;
   padding: 1.0625rem 1.25rem;
   margin-top: 3.5rem;
@@ -14,5 +12,6 @@ export const STextarea = styled.textarea`
   resize: ${({resize})=> !resize ? 'none' : 'both' };
   &:focus{
     outline: none;
+    border: .0625rem solid #2984CE;
   }
 `
