9aa7a209761d314940ed0478cc5b87718eed744f
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-05 18:37:59 +0400

    wrap buttons with container

--- src/components/Buttons/SaveAddButtons/SaveAddButtons.jsx
+++ src/components/Buttons/SaveAddButtons/SaveAddButtons.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import { Button } from '../../Button/Button'
-import { SAddButton } from './SaveAddButtons.styled'
+import { SAddButton, SSaveAddButtons } from './SaveAddButtons.styled'
 
 export const SaveAddButtons = () => {
   return (
-    <div style={{display: "flex", alignItems: "center", gap:"2.188rem"}}>
+    <SSaveAddButtons>
       <SAddButton>
         <img src="assets/svg/plus2.svg" alt="" />
       </SAddButton>
       <Button width="10.875rem" height="3rem" fontSize="1.125rem">შენახვა</Button>
-    </div>
+    </SSaveAddButtons>
   )
 }
--- src/components/Buttons/SaveAddButtons/SaveAddButtons.styled.js
+++ src/components/Buttons/SaveAddButtons/SaveAddButtons.styled.js
@@ -1,5 +1,11 @@
 import styled from "styled-components";
 
+export const SSaveAddButtons = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2.188rem;
+`
+
 export const SAddButton = styled.button`
 > img {
     cursor: pointer;
