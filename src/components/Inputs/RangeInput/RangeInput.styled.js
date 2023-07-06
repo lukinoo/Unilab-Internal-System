e44e43cc9b1c67ba3d6703ba3211fe2ae0950b3f
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-06 15:40:17 +0400

    add RadioDiv and Line component

--- src/components/Inputs/RangeInput/RangeInput.styled.js
+++ src/components/Inputs/RangeInput/RangeInput.styled.js
@@ -4,7 +4,10 @@ import circleSvg from "/assets/svg/whiteCircle.svg";
 export const SRangeInputContainer = styled.div`
   display: flex;
   color: white;
+  width: 56.4375rem;
   align-items: center;
+  justify-content: space-around;
+  margin: 2rem 0;
 `;
 export const SRadioButton = styled.input`
   all: unset;
@@ -14,11 +17,33 @@ export const SRadioButton = styled.input`
   border-radius: 50%;
   border: .125rem solid #ffffff;
   cursor: pointer;
-  
+
   &:checked{
     background-image: url(${circleSvg});
     background-size: 1.25rem;
     background-position: center;
     background-repeat: no-repeat;
   }
+`
+
+export const SRadioDiv = styled.div`
+  display: flex;
+  position: relative;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 3rem;
+  font-size: .875rem;
+  width: 10rem;
+
+  &>p{
+    position: absolute;
+    top: 100%;
+  }
+`
+
+export const SLine = styled.div`
+  width: ${({width})=> width ? width : '7rem'};
+  height: 0.1rem;
+  background-color: gray;
 `
