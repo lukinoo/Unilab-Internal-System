2019f7f0c6116c56c06e700d5742ad78a6f3f0fc
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-06 14:57:52 +0400

    add stytling for range input

---
+++ src/components/Inputs/RangeInput/RangeInput.styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+import circleSvg from "/assets/svg/whiteCircle.svg";
+
+export const SRangeInputContainer = styled.div`
+  display: flex;
+  color: white;
+  align-items: center;
+`;
+export const SRadioButton = styled.input`
+  all: unset;
+  width: 1.875rem;
+  height: 1.875rem;
+  background-color: transparent;
+  border-radius: 50%;
+  border: .125rem solid #ffffff;
+
+  &:checked{
+    background-image: url(${circleSvg});
+    background-size: 1.25rem;
+    background-position: center;
+    background-repeat: no-repeat;
+  }
+`
