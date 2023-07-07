f6ff6d10af7b5d9c3d7592c51c1fe87b23a3682a
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-07 10:56:16 +0400

    format rangeInput file

--- src/components/Inputs/RangeInput/RangeInput.jsx
+++ src/components/Inputs/RangeInput/RangeInput.jsx
@@ -1,23 +1,24 @@
 import { SRadioButton, SRangeInputContainer, SRadioDiv, SLine } from "./RangeInput.styled";
- export const RangeInput = ({
-    rangeValue,
-    name,
-    lastValueName,
-    firstValueName,
- }) => {
+
+export const RangeInput = ({
+  rangeValue,
+  name,
+  lastValueName,
+  firstValueName,
+}) => {
   const loopArray = Array.from({ length: rangeValue }, (_, index) => index + 1);
   return (
     <SRangeInputContainer>
       {loopArray.map((_, index) => (
         <>
-        <SRadioDiv>
-          <SRadioButton type="radio" name={name} key={index} />
-          {index === 0 && <p>{firstValueName}</p>}
-          {index === loopArray.length-1 && <p>{lastValueName}</p>}
-        </SRadioDiv>
-        {index !== loopArray.length-1 &&  <SLine />}
+          <SRadioDiv>
+            <SRadioButton type="radio" name={name} key={index} />
+            {index === 0 && <p>{firstValueName}</p>}
+            {index === loopArray.length - 1 && <p>{lastValueName}</p>}
+          </SRadioDiv>
+          {index !== loopArray.length - 1 && <SLine />}
         </>
       ))}
     </SRangeInputContainer>
   );
-}
+};
