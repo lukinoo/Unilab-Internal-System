c06823464871f80d2236b1a929dc5c98e170df2e
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-14 00:50:15 +0400

    comment out all components; (leave them for reference); Display 3 form pages with proper styles

--- src/views/AddQuestions/AddQuestions.jsx
+++ src/views/AddQuestions/AddQuestions.jsx
@@ -12,87 +12,110 @@ import { SaveAddButtons } from "../../components/Buttons/SaveAddButtons";
 import { RangeInput } from "../../components/Inputs/RangeInput/RangeInput";
 import { RangeLabelInput } from "../../components/Inputs/RangeLabelInput";
 
-const items = {
-  1: "Checkbox",
-  2: "Multiple Choice",
-  3: "Textbox",
-  4: "Rating Scale",
-};
+import { CheckboxForm } from "../../components/FormPages/CheckboxForm";
+import { MultipleChoiceForm } from "../../components/FormPages/MultipleChoiceForm";
+import { TextBoxForm } from "../../components/FormPages/TextBoxForm";
+import { SAddQuestionsMainDiv, STitle, SAddQuestionsContainer } from "./AddQuestions.styled";
 
-const rangeValues = [ 3, 4, 5, 6, 7, 8, 9];
+// const items = {
+//   1: "Checkbox",
+//   2: "Multiple Choice",
+//   3: "Textbox",
+//   4: "Rating Scale",
+// };
 
-export const AddQuestions = () =>{
-  const [questionTypeId, setQuestionTypeId] = useState(1);
+// const rangeValues = [ 3, 4, 5, 6, 7, 8, 9];
 
-  const [firstRangeLabel, setFirstRangeLabel] = useState('');
-  const [secondRangeLabel, setSecondRangeLabel] = useState('');
+// export const AddQuestions = () =>{
+//   const [questionTypeId, setQuestionTypeId] = useState(1);
 
-  const [answers, setAnswers] = useState([
-    { id: "1", content: "" },
-    { id: "2", content: "" },
-    { id: "3", content: "" },
-  ]);
-  const [rangeValue, setRangeValue] = useState(0);
+//   const [firstRangeLabel, setFirstRangeLabel] = useState('');
+//   const [secondRangeLabel, setSecondRangeLabel] = useState('');
+
+//   const [answers, setAnswers] = useState([
+//     { id: "1", content: "" },
+//     { id: "2", content: "" },
+//     { id: "3", content: "" },
+//   ]);
+//   const [rangeValue, setRangeValue] = useState(0);
   
-  const { control } = useForm(); // just a test code for textarea
+//   const { control } = useForm(); // just a test code for textarea
 
-  const addAnswer = () => {
-    const ids = answers.map(obj => obj.id);
-    const largestId = Math.max(...ids) | 0;
-    setAnswers((prevAnswers) => [...prevAnswers, { id: `${largestId+1}`, content: "" }]);
-  };
+//   const addAnswer = () => {
+//     const ids = answers.map(obj => obj.id);
+//     const largestId = Math.max(...ids) | 0;
+//     setAnswers((prevAnswers) => [...prevAnswers, { id: `${largestId+1}`, content: "" }]);
+//   };
   
-  const deleteAnswer = (id) => {
-    const newAnswers = [...answers].filter((answer) => answer.id !== id);
-    setAnswers(newAnswers);
-  };
+//   const deleteAnswer = (id) => {
+//     const newAnswers = [...answers].filter((answer) => answer.id !== id);
+//     setAnswers(newAnswers);
+//   };
 
-  return (
-    <>
-      <h1>Add Questions Page</h1>
-      <QuestionInput />
-      <QuestionTypeDropdown
-        action={setQuestionTypeId}
-        value={questionTypeId}
-        items={items}
-        showImgs={true}
-      />
-      <QuestionTypeDropdown
-        action={setRangeValue}
-        value={rangeValue}
-        items={rangeValues}
-        listWidth="100%"
-        labelText="Range"
-      />
-      <DraggableAnswersContainer
-        answers={answers}
-        setAnswers={setAnswers}
-        type={items[questionTypeId]}
-        deleteAnswer={deleteAnswer}
-      />
-      <TextareaAnswer
-        control={control}
-        name="textAnswer"
-      />
-      <QuestionDescription />
-      <Button
-        width="13.5rem"
-        height="3rem"
-        fontSize=".875rem"
-        LeftComponent={<SPlusIcon src={plusSvg} alt="" />}
-        onClick={addAnswer}
-      >
-        პასუხის დამატება
-      </Button>
-      <RangeLabelInput displayLabel number={1} action={setFirstRangeLabel} />
-      <RangeLabelInput number={rangeValues[rangeValue]} action={setSecondRangeLabel} />
-      <RangeInput
-        rangeValue={rangeValues[rangeValue]}
-        name="rangeValue"
-        firstRangeLabel={firstRangeLabel}
-        secondRangeLabel={secondRangeLabel}
-      />
-      <SaveAddButtons />
-    </>
-  );
-};
+//   return (
+//     <>
+//       <h1>Add Questions Page</h1>
+//       <QuestionInput />
+//       <QuestionTypeDropdown
+//         action={setQuestionTypeId}
+//         value={questionTypeId}
+//         items={items}
+//         showImgs={true}
+//       />
+//       <QuestionTypeDropdown
+//         action={setRangeValue}
+//         value={rangeValue}
+//         items={rangeValues}
+//         listWidth="100%"
+//         labelText="Range"
+//       />
+//       <DraggableAnswersContainer
+//         answers={answers}
+//         setAnswers={setAnswers}
+//         type={items[questionTypeId]}
+//         deleteAnswer={deleteAnswer}
+//       />
+//       <TextareaAnswer
+//         control={control}
+//         name="textAnswer"
+//       />
+//       <QuestionDescription />
+//       <Button
+//         width="13.5rem"
+//         height="3rem"
+//         fontSize=".875rem"
+//         LeftComponent={<SPlusIcon src={plusSvg} alt="" />}
+//         onClick={addAnswer}
+//       >
+//         პასუხის დამატება
+//       </Button>
+//       <RangeLabelInput displayLabel number={1} action={setFirstRangeLabel} />
+//       <RangeLabelInput number={rangeValues[rangeValue]} action={setSecondRangeLabel} />
+//       <RangeInput
+//         rangeValue={rangeValues[rangeValue]}
+//         name="rangeValue"
+//         firstRangeLabel={firstRangeLabel}
+//         secondRangeLabel={secondRangeLabel}
+//       />
+//       <SaveAddButtons />
+//     </>
+//   );
+// };
+
+
+export const AddQuestions = () => {
+  return(
+    <SAddQuestionsMainDiv>
+      {/* HEADER GOES HERE */}
+      {/* SIDEBAR GOES HERE */}
+      <STitle>კითხვების შედგენა</STitle>
+      <SAddQuestionsContainer>
+        <CheckboxForm />
+        <MultipleChoiceForm />
+        <TextBoxForm />
+        {/* Rating scale goes HERE */}
+        <SaveAddButtons />
+      </SAddQuestionsContainer>
+    </SAddQuestionsMainDiv>
+  )
+}
