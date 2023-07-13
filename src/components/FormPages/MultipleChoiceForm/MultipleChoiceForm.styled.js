36dbf021cf33c9cedf7545272b69bffda9ea864a
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-14 00:22:07 +0400

    style SFormbodyDiv

--- src/components/FormPages/MultipleChoiceForm/MultipleChoiceForm.styled.js
+++ src/components/FormPages/MultipleChoiceForm/MultipleChoiceForm.styled.js
@@ -1,1 +1,13 @@
 import styled from "styled-components";
+
+export const SFormBodyDiv = styled.div`
+  position: relative;
+  height: 15rem;
+  display:flex;
+  flex-direction: column;
+  justify-content: space-between;
+  &>button{
+    margin-left: 5rem;
+    margin-top: .875rem;
+  }
+`
