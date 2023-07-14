7b822813af52455b105cf345e8a0a5adb0edfe1b
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-14 18:30:36 +0400

    add handleAddButton function to button that adds new question forms

--- src/components/Buttons/SaveAddButtons/SaveAddButtons.jsx
+++ src/components/Buttons/SaveAddButtons/SaveAddButtons.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 import { Button } from '../../Button/Button'
 import { SAddButton, SSaveAddButtons } from './SaveAddButtons.styled'
 
-export const SaveAddButtons = () => {
+export const SaveAddButtons = ({handleAddQuestion, }) => {
   return (
     <SSaveAddButtons>
-      <SAddButton>
+      <SAddButton onClick={handleAddQuestion}>
         <img src="assets/svg/plus2.svg" alt="" />
       </SAddButton>
       <Button width="10.875rem" height="3rem" fontSize="1.125rem">შენახვა</Button>
