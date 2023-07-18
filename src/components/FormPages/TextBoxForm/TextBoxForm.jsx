5d4fef3f772091edbc62aa96f8a5e37bac829020
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-18 23:17:06 +0400

    add props to TextBoxForm; add function to handle form type change

--- src/components/FormPages/TextBoxForm/TextBoxForm.jsx
+++ src/components/FormPages/TextBoxForm/TextBoxForm.jsx
@@ -6,14 +6,23 @@ import { SFormContainer } from "../CheckboxForm/CheckboxForm.styled";
 import { STextboxDiv } from "./TextBoxForm.styled";
 
 export const TextBoxForm = ({
-  handleRemoveForm
+  formIndex,
+  handleRemoveForm,
+  indexedFormTypes,
+  handleFormTypeChange
 }) => {
   const [questionTypeId, setQuestionTypeId] = useState(3); // Temporary code
   const { control } = useForm(); // just a test code for textarea
 
+  const setFormType = (newFormTypeId) =>{
+    handleFormTypeChange(formIndex, newFormTypeId);
+  }
+
   return (
     <SFormContainer>
       <FormHeader
+        indexedFormTypes={indexedFormTypes}
+        setFormType={setFormType}
         questionTypeId={questionTypeId}
         setQuestionTypeId={setQuestionTypeId}
         handleRemoveForm={handleRemoveForm}
