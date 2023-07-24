1f6c36ab33d21b25c2a9ffe6f2b7804e515b5f55
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-25 01:17:22 +0400

    add dybamic onClick design to duplicate button

--- src/components/Buttons/DuplicateDeleteButtons/DuplicateDeleteButtons.jsx
+++ src/components/Buttons/DuplicateDeleteButtons/DuplicateDeleteButtons.jsx
@@ -1,18 +1,50 @@
-import React from 'react'
-import { SDeleteButtonContainer, SDuplicateButtonContainer, SDuplicateDeleteButtons } from './DuplicateDeleteButtons.styled'
+import React, { useEffect, useState } from "react";
+import {
+  SDeleteButtonContainer,
+  SDuplicateButtonContainer,
+  SDuplicateDeleteButtons,
+} from "./DuplicateDeleteButtons.styled";
+
+const buttonId = "duplicateButton";
 
 export const DuplicateDeleteButtons = ({
   handleRemoveForm,
-  handleCopyForm
+  handleCopyForm,
 }) => {
+  const [isClicked, setisClicked] = useState(false);
+  const srcDuplicate = isClicked
+    ? "assets/svg/duplicateClicked.svg"
+    : "assets/svg/duplicate.svg";
+  console.log(isClicked);
+  useEffect(() => {
+    const handleDesignChange = (e) => {
+      if (e.target.id !== buttonId) {
+        console.log("A");
+        setisClicked(false);
+      }
+    };
+
+    window.addEventListener("click", handleDesignChange);
+    return () => {
+      window.removeEventListener("click", handleDesignChange);
+    };
+  }, [isClicked]);
   return (
     <SDuplicateDeleteButtons>
       <SDuplicateButtonContainer>
-        <img src="assets/svg/duplicate.svg" alt="" onClick={handleCopyForm}/>
+        <img
+          id={buttonId}
+          src={srcDuplicate}
+          alt="Duplicate Form"
+          onClick={() => {
+            handleCopyForm;
+            setisClicked(!isClicked);
+          }}
+        />
       </SDuplicateButtonContainer>
       <SDeleteButtonContainer>
-        <img src="assets/svg/delete.svg" alt="" onClick={handleRemoveForm}/>
+        <img src="assets/svg/delete.svg" alt="" onClick={handleRemoveForm} />
       </SDeleteButtonContainer>
     </SDuplicateDeleteButtons>
-  )
-}
+  );
+};
