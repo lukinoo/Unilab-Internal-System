6eeb310f4fe58e4dddfbec5dcdc92320d35772e2
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-26 00:34:05 +0400

    add test sideBarData object

---
+++ src/components/SideBar/sideBarData.js
@@ -0,0 +1,25 @@
+import testIcon from "/assets/svg/checkbox.svg"
+
+export const sideBarData = [
+  {
+    to: "",
+    icon: testIcon,
+    name: "TEST ICON",
+    label:"",
+    // children: [],
+  },
+  {
+    to: "",
+    icon: testIcon,
+    name: "TEST ICON2",
+    label:"",
+    // children: [],
+  },
+  {
+    to: "",
+    icon: testIcon,
+    name: "TEST ICON3",
+    label:"",
+    // children: [],
+  }
+]
