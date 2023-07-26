30765350b43a283fa79874081c358213bfce256c
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 00:40:01 +0400

    add styled div and title for Portfolio page

---
+++ src/views/Portfolio/Portfolio.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { SCourseTitle } from "../EditCourse/EditCourse.styled";
+
+export const SPortfolioMainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+`
+
+export const STitle = styled(SCourseTitle)`
+`
