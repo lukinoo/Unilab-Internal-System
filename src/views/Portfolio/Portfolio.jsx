5b6ef94a1da0dbbc546b0975c86b575c5b9e5a80
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 00:39:42 +0400

    display portfolio div and a title

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -1,5 +1,9 @@
+import { SPortfolioMainDiv, STitle } from "./Portfolio.styled";
+
 export const Portfolio = () => {
   return (
-    <></>
+    <SPortfolioMainDiv>
+      <STitle>პორტფოლიო</STitle>
+    </SPortfolioMainDiv>
   )
 }
