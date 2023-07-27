f2c6e3731d201b0d022e3caedca52630cd7716d7
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 19:55:50 +0400

    add contactInfoDiv,contactInfoP and SParagraph components; Format code

--- src/components/PortfolioFooter/PortfolioFooter.jsx
+++ src/components/PortfolioFooter/PortfolioFooter.jsx
@@ -1,7 +1,15 @@
-import { SPorfolioFooterDiv, SCircledSocialDiv } from "./PortfolioFooter.styled";
+import {
+  SPorfolioFooterDiv,
+  SCircledSocialDiv,
+  SParagraph,
+  SContactInfoDiv,
+  SContactInfoP,
+} from "./PortfolioFooter.styled";
 import { SSecondaryTiTle } from "../../views/Portfolio/Portfolio.styled";
 import { SocialIcon } from "../SocialIcon";
 import { withCircle } from "../../hocs/withCircle";
+import phoneSvg from "/assets/svg/phone.svg";
+import mailSvg from "/assets/svg/mail.svg";
 
 const CircledIcon = withCircle(SocialIcon);
 
@@ -9,16 +17,19 @@ export const PortfolioFooter = ({ gmail, number, links }) => {
   return (
     <SPorfolioFooterDiv>
       <SSecondaryTiTle>კონტაქტი</SSecondaryTiTle>
-      <p>დაინტერესდი ჩემთან კოლაბორაციით? მზად ვარ ახალი გამოწვევებისთვის</p>
-      <div>
-        <p>+995 333 67 56 43</p>
-        <p>contact@contact.ge</p>
-      </div>
+      <SParagraph>
+        <span>დაინტერესდი ჩემთან კოლაბორაციით?</span>
+        <span>მზად ვარ ახალი გამოწვევებისთვის</span>
+      </SParagraph>
+      <SContactInfoDiv>
+        <SContactInfoP img={phoneSvg}>+995 333 67 56 43</SContactInfoP>
+        <SContactInfoP img={mailSvg}>contact@contact.ge</SContactInfoP>
+      </SContactInfoDiv>
       <SCircledSocialDiv>
-        <CircledIcon social='github' link={links.github}/>
-        <CircledIcon social='facebook' link={links.facebook} />
-        <CircledIcon social='linkedin' link={links.linkedin} />
-        <CircledIcon social='instagram' link={links.instagram} />
+        <CircledIcon social="github" link={links.github} />
+        <CircledIcon social="facebook" link={links.facebook} />
+        <CircledIcon social="linkedin" link={links.linkedin} />
+        <CircledIcon social="instagram" link={links.instagram} />
       </SCircledSocialDiv>
     </SPorfolioFooterDiv>
   );
