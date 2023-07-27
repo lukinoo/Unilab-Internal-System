93f239b6c41cbbce832d35e6af842bfef71438ed
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 02:36:41 +0400

    add ProjectInfoContainer styles; Add border to Container

--- src/components/ProjectInfo/ProjectInfo.styled.js
+++ src/components/ProjectInfo/ProjectInfo.styled.js
@@ -1,11 +1,18 @@
-import styled from "styled-components"
+import styled from "styled-components";
 
 export const SProjectInfoContainer = styled.div`
+  position: relative;
   display: flex;
-`
+  align-items: center;
+  height: 19.5625rem;
+  width: 67.375rem;
+  background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='30' ry='30' stroke='%233F48CC' stroke-width='2' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+`;
 export const SDateDiv = styled.div`
-
-`
+  position: absolute;
+  transform: rotate(-90deg);
+  left: -3.75rem;
+`;
 export const SProjectInfoDiv = styled.div`
-  
-`
+  border-radius: 1.875rem;
+`;
