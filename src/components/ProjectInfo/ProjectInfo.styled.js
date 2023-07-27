2787e68ff7cc42aeaf1d069b8297e02df4d0c0aa
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 02:17:04 +0400

    export some styled components

---
+++ src/components/ProjectInfo/ProjectInfo.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+export const SProjectInfoContainer = styled.div`
+  display: flex;
+`
+export const SDateDiv = styled.div`
+
+`
+export const SProjectInfoDiv = styled.div`
+  
+`
