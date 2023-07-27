4c5c3feb8d3ec54cd0d102b7c3f29e2ace4c7eca
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 12:19:51 +0400

    export styled component variables for InternInfoContainer

--- src/components/InternInfoContainer/InternInfoContainer.styled.js
+++ src/components/InternInfoContainer/InternInfoContainer.styled.js
@@ -1,5 +1,19 @@
 import styled from "styled-components";
 
 export const SInternInfoContainer = styled.div`
+`
 
+export const SNameAndOccupationDiv = styled.div`
+`
+
+export const SName = styled.p`
+`
+
+export const SOccupation = styled(SName)`
+`
+
+export const SDescription = styled.p`
+`
+
+export const SImage = styled.img`
 `
