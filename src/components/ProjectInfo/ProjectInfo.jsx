35aa419b7baaa682938929ef12a148df6c615249
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 02:16:50 +0400

    add page structure of ProjectInfo

--- src/components/ProjectInfo/ProjectInfo.jsx
+++ src/components/ProjectInfo/ProjectInfo.jsx
@@ -1,5 +1,21 @@
-export const ProjectInfo = () => {
+import { SProjectInfoContainer, SDateDiv, SProjectInfoDiv} from "./ProjectInfo.styled"
+
+export const ProjectInfo = ({projectObj}) => {
   return (
-    <></>
+    <SProjectInfoContainer>
+      <SDateDiv>{projectObj.startDate}-{projectObj.endDate}</SDateDiv>
+      <SProjectInfoDiv>
+        {/* image */}
+        <div>
+          <div>
+            {/* title */}
+            {/* role */}
+            {/* status */}
+          </div>
+          {/* description */}
+        </div>
+        {/* button */}
+      </SProjectInfoDiv>
+    </SProjectInfoContainer>
   )
 }
