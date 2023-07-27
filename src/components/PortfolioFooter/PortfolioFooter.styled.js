1d69e5510dc5e9bc1360ba8ded966da819ad6cd0
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 15:55:13 +0400

    remove red bg

--- src/components/PortfolioFooter/PortfolioFooter.styled.js
+++ src/components/PortfolioFooter/PortfolioFooter.styled.js
@@ -4,7 +4,6 @@ export const SPorfolioFooterDiv = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  background-color: red;
   margin-top: 20rem;// temporary
   color: #ffffff;
   font-size: 1.125rem;
