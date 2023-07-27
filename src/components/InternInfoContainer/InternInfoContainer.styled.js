ec11c1db5a00614119af3ce88bd0d49680bf31ed
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 13:00:07 +0400

    add some styling for internInfoContainer components

--- src/components/InternInfoContainer/InternInfoContainer.styled.js
+++ src/components/InternInfoContainer/InternInfoContainer.styled.js
@@ -1,19 +1,57 @@
 import styled from "styled-components";
 
 export const SInternInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: #ffffff;
 `
 
 export const SNameAndOccupationDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  height: 4rem;
 `
 
 export const SName = styled.p`
+  color: #3F48CC;
+  font-size: 1.5rem;
+  font-weight: 600;
 `
 
 export const SOccupation = styled(SName)`
+  font-size: 1.125rem;
 `
 
 export const SDescription = styled.p`
+  font-size: 1rem;
 `
 
-export const SImage = styled.img`
+export const SImageDiv = styled.div`
+  border: .0625rem dashed #3F48CC;
+  border-radius: 1.875rem;
+  padding: 1.5rem;
+  & > img {
+    border-radius: 1rem;
+    width: 14.9375rem;
+    height: 17.9375rem;
+    object-fit: cover;
+  }
+`;
+export const SImageAndTextDiv = styled.div`
+  width: 50%;
+  /* max width */
+  display: flex;
+  /* align-items: flex-end; */
+  justify-content: space-between;
+`
+
+export const SInfoDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  background-color: red;
+  width: 36.1875rem;
+
 `
