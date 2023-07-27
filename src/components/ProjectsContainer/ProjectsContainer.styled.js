3c222415615ce97f7fd4ae5d5d32fba945585473
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 02:37:01 +0400

    add basic styles to ProjectsContainer

--- src/components/ProjectsContainer/ProjectsContainer.styled.js
+++ src/components/ProjectsContainer/ProjectsContainer.styled.js
@@ -4,5 +4,13 @@ export const SProjectsContainer = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  min-height: 60rem;
+  justify-content: space-between;
   color: #ffffff;
+`
+export const SProjectsList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
 `
