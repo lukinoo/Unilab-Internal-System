a2fe4a43547b8dae1853cbd683f2b4092c46a974
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 02:05:34 +0400

    add variables and function to display correct amount of projects

--- src/components/ProjectsContainer/ProjectsContainer.jsx
+++ src/components/ProjectsContainer/ProjectsContainer.jsx
@@ -1,14 +1,33 @@
 import { SProjectsContainer } from "./ProjectsContainer.styled";
 import { SSecondaryTiTle } from "../../views/Portfolio/Portfolio.styled";
+import { useState } from "react";
 
 const PROJECTS_PER_PAGE = 4;
 
 export const ProjectsContainer = ({
   projects
 }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const startIndex = currentPage * PROJECTS_PER_PAGE;
+  const endIndex = (currentPage + 1) * PROJECTS_PER_PAGE;
+  const pageCount = Math.ceil(projects.length / PROJECTS_PER_PAGE);
+  
+  const getBatchData = (start, end) => {
+    if (end === 0) {
+      // corner case
+      return [...projects.slice(start, -1), projects[data.length - 1]];
+    }
+    return projects.slice(start - PROJECTS_PER_PAGE, end - PROJECTS_PER_PAGE);
+  };
+
   return (
     <SProjectsContainer>
       <SSecondaryTiTle>პროექტები და გამოცდილება</SSecondaryTiTle>
+      {getBatchData(startIndex, endIndex).map((projectObj)=>(
+        <>
+          
+        </>
+      ))}
     </SProjectsContainer>
   )
 }
