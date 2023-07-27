a8b309a8e1ed2e484938a42cc3f15dcef2399e03
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 20:14:11 +0400

    add SProjectContainer

--- src/components/ProjectsContainer/ProjectsContainer.jsx
+++ src/components/ProjectsContainer/ProjectsContainer.jsx
@@ -1,5 +1,14 @@
-export const ProjectsContainer = () => {
+import { SProjectsContainer } from "./ProjectsContainer.styled";
+import { SSecondaryTiTle } from "../../views/Portfolio/Portfolio.styled";
+
+const PROJECTS_PER_PAGE = 4;
+
+export const ProjectsContainer = ({
+  projects
+}) => {
   return (
-    <></>
+    <SProjectsContainer>
+      <SSecondaryTiTle>პროექტები და გამოცდილება</SSecondaryTiTle>
+    </SProjectsContainer>
   )
 }
