568df6e922805a85c1f229b3ae1be245d98b0830
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 13:02:56 +0400

    format doc

--- src/components/InternInfoContainer/InternInfoContainer.styled.js
+++ src/components/InternInfoContainer/InternInfoContainer.styled.js
@@ -5,31 +5,31 @@ export const SInternInfoContainer = styled.div`
   flex-direction: column;
   align-items: center;
   color: #ffffff;
-`
+`;
 
 export const SNameAndOccupationDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   height: 4rem;
-`
+`;
 
 export const SName = styled.p`
-  color: #3F48CC;
+  color: #3f48cc;
   font-size: 1.5rem;
   font-weight: 600;
-`
+`;
 
 export const SOccupation = styled(SName)`
   font-size: 1.125rem;
-`
+`;
 
 export const SDescription = styled.p`
   font-size: 1rem;
-`
+`;
 
 export const SImageDiv = styled.div`
-  border: .0625rem dashed #3F48CC;
+  border: 0.0625rem dashed #3f48cc;
   border-radius: 1.875rem;
   padding: 1.5rem;
   & > img {
@@ -43,11 +43,11 @@ export const SImageAndTextDiv = styled.div`
   width: 59.25rem;
   display: flex;
   justify-content: space-between;
-`
+`;
 
 export const SInfoDiv = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-around;
   width: 36.1875rem;
-`
+`;
