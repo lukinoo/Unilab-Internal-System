8c2cbf288ddfd6c9bc5f79e4eb1163aaf101f62c
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 15:36:44 +0400

    display socialIcons and texts at PorfolioFooter

--- src/components/PortfolioFooter/PortfolioFooter.jsx
+++ src/components/PortfolioFooter/PortfolioFooter.jsx
@@ -1,11 +1,22 @@
 import { SPorfolioFooterDiv } from "./PortfolioFooter.styled";
 import { SSecondaryTiTle } from "../../views/Portfolio/Portfolio.styled";
+import { SocialIcon } from "../SocialIcon";
 
 export const PortfolioFooter = ({ gmail, number, links }) => {
   return (
     <SPorfolioFooterDiv>
       <SSecondaryTiTle>კონტაქტი</SSecondaryTiTle>
-      
+      <p>დაინტერესდი ჩემთან კოლაბორაციით? მზად ვარ ახალი გამოწვევებისთვის</p>
+      <div>
+        <p>+995 333 67 56 43</p>
+        <p>contact@contact.ge</p>
+      </div>
+      <div>
+        <SocialIcon social='github' link={links.github} />
+        <SocialIcon social='facebook' link={links.facebook} />
+        <SocialIcon social='linkedin' link={links.linkedin} />
+        <SocialIcon social='instagram' link={links.instagram} />
+      </div>
     </SPorfolioFooterDiv>
   );
 };
