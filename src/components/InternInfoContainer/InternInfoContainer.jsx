a2359e3ec87af2e58bf670b0bc5c15ceea6c483a
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 12:19:27 +0400

    add starting page structure

--- src/components/InternInfoContainer/InternInfoContainer.jsx
+++ src/components/InternInfoContainer/InternInfoContainer.jsx
@@ -1,5 +1,29 @@
-export const InternInfoContainer = () => {
+import { SInternInfoContainer, SNameAndOccupationDiv, SName, SOccupation, SDescription, SImage  } from "./InternInfoContainer.styled";
+
+export const InternInfoContainer = ({
+  name,
+  occupation,
+  description,
+  imgUrl,
+  links // Social Links object
+}) => {
   return (
-    <></>
+    <SInternInfoContainer>
+      <div>
+
+        <SImage src={imgUrl} alt={name} />
+        <div>
+          <SNameAndOccupationDiv>
+              <SName>{name}</SName>
+              <SOccupation>{occupation}</SOccupation>
+          </SNameAndOccupationDiv>
+          <SDescription>
+            {description}
+          </SDescription>
+        </div>
+      </div>
+      {/* Button here */}
+      {/* Social Links */}
+    </SInternInfoContainer>
   )
 }
