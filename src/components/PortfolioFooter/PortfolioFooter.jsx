17a999783f9539ab76081a4610227e50548f53e2
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 15:29:47 +0400

    add SPortfolioFooterDiv and Secondary title to Portfolio Footer

--- src/components/PortfolioFooter/PortfolioFooter.jsx
+++ src/components/PortfolioFooter/PortfolioFooter.jsx
@@ -1,5 +1,11 @@
-export const PortfolioFooter = () => {
+import { SPorfolioFooterDiv } from "./PortfolioFooter.styled";
+import { SSecondaryTiTle } from "../../views/Portfolio/Portfolio.styled";
+
+export const PortfolioFooter = ({ gmail, number, links }) => {
   return (
-    <></>
-  )
-}
+    <SPorfolioFooterDiv>
+      <SSecondaryTiTle>კონტაქტი</SSecondaryTiTle>
+      
+    </SPorfolioFooterDiv>
+  );
+};
