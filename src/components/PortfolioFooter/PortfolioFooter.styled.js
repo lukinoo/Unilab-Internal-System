26f69b89ce47ed64776ae43b949cde084f77b14e
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 15:30:01 +0400

    add SPortfolioFooter styled div

---
+++ src/components/PortfolioFooter/PortfolioFooter.styled.js
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const SPorfolioFooterDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: red;
+  margin-top: 20rem;// temporary
+`;
