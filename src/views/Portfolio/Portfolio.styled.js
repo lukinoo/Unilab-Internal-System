d608f10af37f3fddcb2e1e735980f970d1883c24
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-27 15:16:56 +0400

    remove width:100vw from SPortfolioMainDiv because it caused overflow-x

--- src/views/Portfolio/Portfolio.styled.js
+++ src/views/Portfolio/Portfolio.styled.js
@@ -4,7 +4,6 @@ import { SCourseTitle } from "../EditCourse/EditCourse.styled";
 export const SPortfolioMainDiv = styled.div`
   display: flex;
   flex-direction: column;
-  width: 100vw;
   height: 100vh;
 `
 
