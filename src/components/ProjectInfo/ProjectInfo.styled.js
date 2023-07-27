aa96c38724b3c43da8ce106895dffe979f618025
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 02:41:22 +0400

    add margin to ProjectInfoContainer

--- src/components/ProjectInfo/ProjectInfo.styled.js
+++ src/components/ProjectInfo/ProjectInfo.styled.js
@@ -7,6 +7,7 @@ export const SProjectInfoContainer = styled.div`
   height: 19.5625rem;
   width: 67.375rem;
   background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='30' ry='30' stroke='%233F48CC' stroke-width='2' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+  margin: 3.0625rem 0;
 `;
 export const SDateDiv = styled.div`
   position: absolute;
