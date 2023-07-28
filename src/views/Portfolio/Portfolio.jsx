989c8cad290732d5595f75656aa632c5b68056a9
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 10:56:45 +0400

    add urls property to project objects

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -27,7 +27,8 @@ export const Portfolio = () => {
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
-          status: 'პრაქტიკული პროექტი'
+          status: 'პრაქტიკული პროექტი',
+          url: '',
         },
         {
           title: "პროექტის დასახელება",
@@ -36,7 +37,8 @@ export const Portfolio = () => {
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
-          status: 'საფინალო პროექტი'
+          status: 'საფინალო პროექტი',
+          url: ''
         },
         {
           title: "პროექტის დასახელება",
@@ -45,7 +47,8 @@ export const Portfolio = () => {
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
-          status: 'საფინალო პროექტი'
+          status: 'საფინალო პროექტი',
+          url: ''
         },
         {
           title: "პროექტის დასახელება",
@@ -54,7 +57,8 @@ export const Portfolio = () => {
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
-          status: 'საფინალო პროექტი'
+          status: 'საფინალო პროექტი',
+          url: ''
         },
         {
           title: "პროექტის დასახელება",
@@ -63,7 +67,8 @@ export const Portfolio = () => {
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
-          status: 'პრაქტიკული პროექტი'
+          status: 'პრაქტიკული პროექტი',
+          url: ''
         },
       ]}/>
       {/* Projects pagination */}
