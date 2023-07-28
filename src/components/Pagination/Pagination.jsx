f36423c9632de408756b86ea579f4857198a9e61
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 13:44:11 +0400

    remove actions array and a console.log line

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,13 +1,11 @@
 import { SPaginationDiv, SPageNumber } from "./Pagination.styled";
 
-const ACTIONS = [-2, -1, 0, 1, 2];
 export const Pagination = ({
   handlePageChange,
   currentPage,
   totalPages,
 }) => {
   const numbersArray = Array.from({ length: totalPages }, (_, index) => index + 1);
-  console.log("NUMBERS ARR:",numbersArray)
   return (
     <SPaginationDiv>
       {numbersArray.map((number) => (
