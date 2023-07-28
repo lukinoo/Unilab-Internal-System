576a8393f03ef33a7879f7353746285a694c1a67
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 13:42:50 +0400

    add logic for pagination with numbers

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,5 +1,24 @@
-export const Pagination = () => {
+import { SPaginationDiv, SPageNumber } from "./Pagination.styled";
+
+const ACTIONS = [-2, -1, 0, 1, 2];
+export const Pagination = ({
+  handlePageChange,
+  currentPage,
+  totalPages,
+}) => {
+  const numbersArray = Array.from({ length: totalPages }, (_, index) => index + 1);
+  console.log("NUMBERS ARR:",numbersArray)
   return (
-    <></>
-  )
+    <SPaginationDiv>
+      {numbersArray.map((number) => (
+        <SPageNumber
+          key={number}
+          active={number === currentPage}
+          onClick={() => handlePageChange(number)}
+        >
+          {number}
+        </SPageNumber>
+      ))}
+    </SPaginationDiv>
+  );
 }
