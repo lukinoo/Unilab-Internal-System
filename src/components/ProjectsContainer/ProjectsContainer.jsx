a413705943ae91b524a5cf21b4f441728b48a645
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 11:27:12 +0400

    add function to handle page change

--- src/components/ProjectsContainer/ProjectsContainer.jsx
+++ src/components/ProjectsContainer/ProjectsContainer.jsx
@@ -2,17 +2,16 @@ import { SProjectsContainer, SProjectsList } from "./ProjectsContainer.styled";
 import { SSecondaryTiTle } from "../../views/Portfolio/Portfolio.styled";
 import { useState } from "react";
 import { ProjectInfo } from "../ProjectInfo/ProjectInfo";
+import { Pagination } from "../Pagination/Pagination";
 
 const PROJECTS_PER_PAGE = 4;
 
-export const ProjectsContainer = ({
-  projects
-}) => {
+export const ProjectsContainer = ({ projects }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const startIndex = currentPage * PROJECTS_PER_PAGE;
   const endIndex = (currentPage + 1) * PROJECTS_PER_PAGE;
-  const pageCount = Math.ceil(projects.length / PROJECTS_PER_PAGE);
-  
+  const totalPages = Math.ceil(projects.length / PROJECTS_PER_PAGE);
+
   const getBatchData = (start, end) => {
     if (end === 0) {
       // corner case
@@ -21,14 +20,25 @@ export const ProjectsContainer = ({
     return projects.slice(start - PROJECTS_PER_PAGE, end - PROJECTS_PER_PAGE);
   };
 
+  const handlePageChange = (pageNumber) => {
+    if (pageNumber <= 0) return;
+    if (totalPages < pageNumber * ITEMS_PER_PAGE) return;
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <SProjectsContainer>
       <SSecondaryTiTle>პროექტები და გამოცდილება</SSecondaryTiTle>
       <SProjectsList>
-        {getBatchData(startIndex, endIndex).map((projectObj)=>(
-          <ProjectInfo projectObj={projectObj}/>
+        {getBatchData(startIndex, endIndex).map((projectObj) => (
+          <ProjectInfo projectObj={projectObj} />
         ))}
       </SProjectsList>
+      <Pagination
+        handlePageChange={handlePageChange}
+        currentPage={currentPage}
+        totalPages={totalPages}
+      />
     </SProjectsContainer>
-  )
-}
+  );
+};
