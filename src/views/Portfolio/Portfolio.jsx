29e06ea60774ff705ed7297c58171777be2aacbc
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 10:30:50 +0400

    add updated projects data

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -24,7 +24,17 @@ export const Portfolio = () => {
           title: "პროექტის დასახელება",
           startDate: 'July 2022',
           endData: 'Present',
-          image: '',
+          image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+          role: 'ჩემი როლი',
+          description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
+          status: 'პრაქტიკული პროექტი'
+        },
+        {
+          title: "პროექტის დასახელება",
+          startDate: 'July 2022',
+          endData: 'Present',
+          image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+          role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
           status: 'საფინალო პროექტი'
         },
@@ -32,7 +42,8 @@ export const Portfolio = () => {
           title: "პროექტის დასახელება",
           startDate: 'July 2022',
           endData: 'Present',
-          image: '',
+          image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+          role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
           status: 'საფინალო პროექტი'
         },
@@ -40,7 +51,8 @@ export const Portfolio = () => {
           title: "პროექტის დასახელება",
           startDate: 'July 2022',
           endData: 'Present',
-          image: '',
+          image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+          role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
           status: 'საფინალო პროექტი'
         },
@@ -48,17 +60,10 @@ export const Portfolio = () => {
           title: "პროექტის დასახელება",
           startDate: 'July 2022',
           endData: 'Present',
-          image: '',
+          image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
+          role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
-          status: 'საფინალო პროექტი'
-        },
-        {
-          title: "პროექტის დასახელება",
-          startDate: 'July 2022',
-          endData: 'Present',
-          image: '',
-          description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
-          status: 'საფინალო პროექტი'
+          status: 'პრაქტიკული პროექტი'
         },
       ]}/>
       {/* Projects pagination */}
