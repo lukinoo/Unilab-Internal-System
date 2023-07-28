0e6d4350d38e3a0599a083c9574a2df991c12bbd
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-07-28 10:49:09 +0400

    fix a typo in projects array

--- src/views/Portfolio/Portfolio.jsx
+++ src/views/Portfolio/Portfolio.jsx
@@ -23,7 +23,7 @@ export const Portfolio = () => {
         {
           title: "პროექტის დასახელება",
           startDate: 'July 2022',
-          endData: 'Present',
+          endDate: 'Present',
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
@@ -32,7 +32,7 @@ export const Portfolio = () => {
         {
           title: "პროექტის დასახელება",
           startDate: 'July 2022',
-          endData: 'Present',
+          endDate: 'Present',
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
@@ -41,7 +41,7 @@ export const Portfolio = () => {
         {
           title: "პროექტის დასახელება",
           startDate: 'July 2022',
-          endData: 'Present',
+          endDate: 'Present',
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
@@ -50,7 +50,7 @@ export const Portfolio = () => {
         {
           title: "პროექტის დასახელება",
           startDate: 'July 2022',
-          endData: 'Present',
+          endDate: 'Present',
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
@@ -59,7 +59,7 @@ export const Portfolio = () => {
         {
           title: "პროექტის დასახელება",
           startDate: 'July 2022',
-          endData: 'Present',
+          endDate: 'Present',
           image: 'https://s3-alpha-sig.figma.com/img/34f9/e4f8/12aac5d63a75264eb5dde9b4ea2d1a95?Expires=1691366400&Signature=H8-MA~CMntYsHY3L4QihwCqHNdfXybKzWDumIj9pMuMHEH7EK6-5ddZwOv6MLUV5FmQeHr-ouUc~mg63LQkwaflpLQda3Rdf3kN5HKXxdPSFJO4E0NRBSLyYQYUoBHjEvzKaRaSSmNjaZvpjgn2mi2yYK~QVY7LDNLauyP~agACPGRz-vZI5tr5fx2O1isI9THiNoUnp7DeF5Eal1ckWjZuvYQJfwkkGpNNSWaFh3fi71Et6H0CU2pY0ucuDczzAIjPRN8u8EsX98F4z8FoY-g-0EdCLIG27j7gUacY3Fh5qJ8r-hHMt5Qm6m4i7xDOnvqpGowhTQNvC174Wt3XISg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4',
           role: 'ჩემი როლი',
           description: 'მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ, მოკლედ პროექტის შესახებ',
