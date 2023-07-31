226087de3b53f732cec01202dfcb28b89130fbe0
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-31 20:21:48 +0400

    implement slider functionality using slick library

--- src/components/CoursesSlider/CoursesSlider.jsx
+++ src/components/CoursesSlider/CoursesSlider.jsx
@@ -1,7 +1,57 @@
-import React from 'react'
+import React from "react";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import {
+  SCoursesSlider,
+  SCoursesSliderCard,
+  SCoursesSliderCardInfo,
+  SCoursesSliderCardVectorLeft,
+  SCoursesSliderCardVectorRight,
+  SCoursesSliderDashedBottom,
+  SCoursesSliderDashedTop,
+  SCoursesSliderImg,
+  SCoursesSliderImgContainer,
+  SCoursesSliderImgWrapper,
+} from "./CoursesSlider.styled";
+import { activeCoursesData } from "../../data";
 
 export const CoursesSlider = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 700,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    initialSlide: 0,
+  };
   return (
-    <div>CoursesSlider</div>
-  )
-}
+    <SCoursesSlider>
+      <Slider {...settings}>
+        {activeCoursesData.map(({ id, title, src }) => (
+          <SCoursesSliderCard key={id}>
+            <SCoursesSliderCardInfo>
+              <SCoursesSliderImgContainer>
+                <SCoursesSliderImgWrapper>
+                  <SCoursesSliderImg src={src} alt={title} />
+                </SCoursesSliderImgWrapper>
+                <SCoursesSliderCardVectorLeft>
+                  <img src="assets/svg/vectorLeft.svg" alt="" />
+                </SCoursesSliderCardVectorLeft>
+                <SCoursesSliderCardVectorRight>
+                  <img src="assets/svg/vectorLeft.svg" alt="" />
+                </SCoursesSliderCardVectorRight>
+                <SCoursesSliderDashedTop>
+                  <img src="assets/svg/dashedTop.svg" alt="" />
+                </SCoursesSliderDashedTop>
+                <SCoursesSliderDashedBottom>
+                  <img src="assets/svg/dashedTop.svg" alt="" />
+                </SCoursesSliderDashedBottom>
+              </SCoursesSliderImgContainer>
+            </SCoursesSliderCardInfo>
+          </SCoursesSliderCard>
+        ))}
+      </Slider>
+    </SCoursesSlider>
+  );
+};
