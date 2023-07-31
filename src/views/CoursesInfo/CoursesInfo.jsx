2446f7ef497f3329666c3fc0cff18ed202d8bc27
vazha2000 <vaja.siqtura2000@gmail.com>
2023-07-31 20:45:29 +0400

    add line component

--- src/views/CoursesInfo/CoursesInfo.jsx
+++ src/views/CoursesInfo/CoursesInfo.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { SActiveCourses, SCoursesInfo, SCoursesInfoContainer } from './CoursesInfo.styled'
+import { FourthHeader } from '../../components/Headers/FourthHeader'
+import { CoursesSlider } from '../../components/CoursesSlider'
+import { SAboutLine } from '../AboutProject/AboutProject.styled'
 
 export const CoursesInfo = () => {
   return (
-    <div>CoursesInfo</div>
+    <SCoursesInfo>
+      <FourthHeader />
+      <SCoursesInfoContainer>
+        <SActiveCourses>
+          <span>აქტიური კონკურსები</span>
+          <CoursesSlider />
+        </SActiveCourses>
+        <SAboutLine marginTop={"9rem"}>
+          <img src="/assets/svg/lineLeft.svg" alt="lineLeft" />
+        </SAboutLine>
+      </SCoursesInfoContainer>
+    </SCoursesInfo>
   )
 }
