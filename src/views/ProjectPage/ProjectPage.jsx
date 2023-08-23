9494b8e1e5ade0eb02b11b59a9d0e66b57e6ea4a
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-23 17:54:19 +0400

    add starting structure of project page; Add projectpagemaindiv component

--- src/views/ProjectPage/ProjectPage.jsx
+++ src/views/ProjectPage/ProjectPage.jsx
@@ -1,7 +1,17 @@
+import { FourthHeader } from "../../components/Headers/FourthHeader"
+import { SProjectPageMainDiv } from "./ProjectPage.styled"
+
 export const ProjectPage = () => {
   return (
-    <>
-      <h2>პროექტი</h2>
-    </>
+    <SProjectPageMainDiv>
+      <FourthHeader />
+      <h2>საფინალო პროექტი</h2>
+      {/* Large Slider */}
+      {/* Project info */}
+      {/* img slider */}
+      {/* My role */}
+      {/* Project link */}
+      {/* Used tools */}
+    </SProjectPageMainDiv>
   )
 }
---
+++ src/views/ProjectPage/ProjectPage.styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const SProjectPageMainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: #ffffff;
+  font-family: 'Helvetica', sans-serif;
+  & * {
+    font-family: inherit;
+  }
+`
