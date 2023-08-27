d7e293d3dbcf1fb203675fc96281f77228a385d2
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-27 20:03:51 +0400

    use TestProjectData instead of ProjectData

--- src/views/ProjectPage/ProjectPage.jsx
+++ src/views/ProjectPage/ProjectPage.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import { FourthHeader } from "../../components/Headers/FourthHeader"
 import { SProjectPageMainDiv } from "./ProjectPage.styled"
 import { ProjectSlider } from "../../components/ProjectSlider"
-import { ProjectData } from "./TestData"
+import { TestProjectData } from "./TestData"
 
 // user = person whose porfolio link was used to load the page
 export const ProjectPage = () => {
@@ -13,7 +13,7 @@ export const ProjectPage = () => {
     setSliderIndex(i);
   };
   const findUserObj = () =>{
-    const userObj = ProjectData.team.filter(obj=>obj.name == user)[0];
+    const userObj = TestProjectData.team.filter(obj=>obj.name == user)[0];
     return userObj;
   }
   const role = findUserObj(user).role;
@@ -23,12 +23,12 @@ export const ProjectPage = () => {
     <SProjectPageMainDiv>
       <FourthHeader />
       <h2>საფინალო პროექტი</h2>
-      <ProjectSlider onChange={handleSliderChange} data={ProjectData.images} imgHeight={'37.5rem'}/>
+      <ProjectSlider onChange={handleSliderChange} data={TestProjectData.images} imgHeight={'37.5rem'}/>
       {/* Project info */}
       <div>
-        <p>პროექტის დასახელება: {ProjectData.name}</p>
+        <p>პროექტის დასახელება: {TestProjectData.name}</p>
         <p>პროექტის აღწერა:</p>
-        <p>{ProjectData.description}</p>
+        <p>{TestProjectData.description}</p>
       </div>
       {/* Project info */}
       {/* img slider */}
@@ -37,9 +37,9 @@ export const ProjectPage = () => {
         <p>ჩემი როლი: {role}</p>
         <p>{roleDescription}</p>
       </div>
-      <p>პროექტის ხანგრძლივობა: {ProjectData.duration}</p>
+      <p>პროექტის ხანგრძლივობა: {TestProjectData.duration}</p>
       {/* Project link */}
       {/* Used tools */}
     </SProjectPageMainDiv>
-  )
+  );
 }
