fdf9c108fbab91a1712624ab7879ac4b1582c117
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-27 14:53:39 +0400

    add page strucure; add user string; add a function to find user role

--- src/views/ProjectPage/ProjectPage.jsx
+++ src/views/ProjectPage/ProjectPage.jsx
@@ -2,38 +2,42 @@ import { useState } from "react"
 import { FourthHeader } from "../../components/Headers/FourthHeader"
 import { SProjectPageMainDiv } from "./ProjectPage.styled"
 import { ProjectSlider } from "../../components/ProjectSlider"
+import { ProjectData } from "./TestData"
 
-const TestData = [
-  {
-    id: 1,
-    src: "https://s3-alpha-sig.figma.com/img/eb02/a5e4/b2909e6a5dc39f47a4ba1841937dc804?Expires=1693785600&Signature=NeG5sDxB4Iya~ZvlmisjgqYzcAr~JlQ43P9IiP1YofSo5wWTP9N2aoc7nKTohQDv22abOwbO8u8d4ESQjOsv9Kp9QiZeCnwaHvj5EYUJEcDrX20zls9c9XawrEE634akopWvG8hy3nhprPFo5uBLhuXQPY~bR94aD4hSGzgzqkFdxMVepc4PMkSrCXJ5UO6a2JJS~Zz~0r3bEhk~yyDnb98mu-4zphmmhzCJ4sftWnoKJJONbhDJSYWyYLa0M6r3Yz2I2ZF4moX6Qv9ma93afwCO7Q3iKC87Z00vmQXQ3Ioxgx7oyGwgD68l6paM~2OLalrW6jnCTnGDMS5BbjKTyg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-  },
-  {
-    id: 2,
-    src: "https://s3-alpha-sig.figma.com/img/eb02/a5e4/b2909e6a5dc39f47a4ba1841937dc804?Expires=1693785600&Signature=NeG5sDxB4Iya~ZvlmisjgqYzcAr~JlQ43P9IiP1YofSo5wWTP9N2aoc7nKTohQDv22abOwbO8u8d4ESQjOsv9Kp9QiZeCnwaHvj5EYUJEcDrX20zls9c9XawrEE634akopWvG8hy3nhprPFo5uBLhuXQPY~bR94aD4hSGzgzqkFdxMVepc4PMkSrCXJ5UO6a2JJS~Zz~0r3bEhk~yyDnb98mu-4zphmmhzCJ4sftWnoKJJONbhDJSYWyYLa0M6r3Yz2I2ZF4moX6Qv9ma93afwCO7Q3iKC87Z00vmQXQ3Ioxgx7oyGwgD68l6paM~2OLalrW6jnCTnGDMS5BbjKTyg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-  },
-  {
-    id: 3,
-    src: "https://s3-alpha-sig.figma.com/img/eb02/a5e4/b2909e6a5dc39f47a4ba1841937dc804?Expires=1693785600&Signature=NeG5sDxB4Iya~ZvlmisjgqYzcAr~JlQ43P9IiP1YofSo5wWTP9N2aoc7nKTohQDv22abOwbO8u8d4ESQjOsv9Kp9QiZeCnwaHvj5EYUJEcDrX20zls9c9XawrEE634akopWvG8hy3nhprPFo5uBLhuXQPY~bR94aD4hSGzgzqkFdxMVepc4PMkSrCXJ5UO6a2JJS~Zz~0r3bEhk~yyDnb98mu-4zphmmhzCJ4sftWnoKJJONbhDJSYWyYLa0M6r3Yz2I2ZF4moX6Qv9ma93afwCO7Q3iKC87Z00vmQXQ3Ioxgx7oyGwgD68l6paM~2OLalrW6jnCTnGDMS5BbjKTyg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
-  },
-];
-
+// user = person whose porfolio link was used to load the page
 export const ProjectPage = () => {
+  const user = 'ნინი წიკლაური'; // temporary string; **receive the user as a prop**
   const [sliderIndex, setSliderIndex] = useState(0);
 
   const handleSliderChange = (i) => {
     setSliderIndex(i);
   };
+  const findRole = () =>{
+    const role = ProjectData.team.filter(obj=>obj.name == user)[0].role;
+    console.log("ROLE:",role);
+    return role;
+  }
+
 
   return (
     <SProjectPageMainDiv>
       <FourthHeader />
       <h2>საფინალო პროექტი</h2>
-      <ProjectSlider onChange={handleSliderChange} data={TestData} imgHeight={'37.5rem'}/>
-      {/* Large Slider */}
+      <ProjectSlider onChange={handleSliderChange} data={ProjectData.images} imgHeight={'37.5rem'}/>
+      {/* Project info */}
+      <div>
+        <p>პროექტის დასახელება: {ProjectData.name}</p>
+        <p>პროექტის აღწერა:</p>
+        <p>{ProjectData.description}</p>
+      </div>
       {/* Project info */}
       {/* img slider */}
       {/* My role */}
+      <div>
+        <p>ჩემი როლი: {findRole(user)}</p>
+        <p>{}</p>
+      </div>
+      <p>პროექტის ხანგრძლივობა: {ProjectData.duration}</p>
       {/* Project link */}
       {/* Used tools */}
     </SProjectPageMainDiv>
