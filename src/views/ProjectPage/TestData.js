580073e4f88443e3def9a4c60b105459657b0dae
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-27 14:39:06 +0400

    move test data to a separate file

---
+++ src/views/ProjectPage/TestData.js
@@ -0,0 +1,53 @@
+export const ProjectData = {
+  name: 'გლოსარიუმი',
+  description: 'პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა, პროექტის აღწერა',
+  images: [
+    {
+      id: 1,
+      src: "https://s3-alpha-sig.figma.com/img/eb02/a5e4/b2909e6a5dc39f47a4ba1841937dc804?Expires=1693785600&Signature=NeG5sDxB4Iya~ZvlmisjgqYzcAr~JlQ43P9IiP1YofSo5wWTP9N2aoc7nKTohQDv22abOwbO8u8d4ESQjOsv9Kp9QiZeCnwaHvj5EYUJEcDrX20zls9c9XawrEE634akopWvG8hy3nhprPFo5uBLhuXQPY~bR94aD4hSGzgzqkFdxMVepc4PMkSrCXJ5UO6a2JJS~Zz~0r3bEhk~yyDnb98mu-4zphmmhzCJ4sftWnoKJJONbhDJSYWyYLa0M6r3Yz2I2ZF4moX6Qv9ma93afwCO7Q3iKC87Z00vmQXQ3Ioxgx7oyGwgD68l6paM~2OLalrW6jnCTnGDMS5BbjKTyg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    },
+    {
+      id: 2,
+      src: "https://s3-alpha-sig.figma.com/img/eb02/a5e4/b2909e6a5dc39f47a4ba1841937dc804?Expires=1693785600&Signature=NeG5sDxB4Iya~ZvlmisjgqYzcAr~JlQ43P9IiP1YofSo5wWTP9N2aoc7nKTohQDv22abOwbO8u8d4ESQjOsv9Kp9QiZeCnwaHvj5EYUJEcDrX20zls9c9XawrEE634akopWvG8hy3nhprPFo5uBLhuXQPY~bR94aD4hSGzgzqkFdxMVepc4PMkSrCXJ5UO6a2JJS~Zz~0r3bEhk~yyDnb98mu-4zphmmhzCJ4sftWnoKJJONbhDJSYWyYLa0M6r3Yz2I2ZF4moX6Qv9ma93afwCO7Q3iKC87Z00vmQXQ3Ioxgx7oyGwgD68l6paM~2OLalrW6jnCTnGDMS5BbjKTyg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    },
+    {
+      id: 3,
+      src: "https://s3-alpha-sig.figma.com/img/eb02/a5e4/b2909e6a5dc39f47a4ba1841937dc804?Expires=1693785600&Signature=NeG5sDxB4Iya~ZvlmisjgqYzcAr~JlQ43P9IiP1YofSo5wWTP9N2aoc7nKTohQDv22abOwbO8u8d4ESQjOsv9Kp9QiZeCnwaHvj5EYUJEcDrX20zls9c9XawrEE634akopWvG8hy3nhprPFo5uBLhuXQPY~bR94aD4hSGzgzqkFdxMVepc4PMkSrCXJ5UO6a2JJS~Zz~0r3bEhk~yyDnb98mu-4zphmmhzCJ4sftWnoKJJONbhDJSYWyYLa0M6r3Yz2I2ZF4moX6Qv9ma93afwCO7Q3iKC87Z00vmQXQ3Ioxgx7oyGwgD68l6paM~2OLalrW6jnCTnGDMS5BbjKTyg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    },
+  ],
+  team: [
+    {
+      name: 'ნინი წიკლაური',
+      img: '',
+      role: 'პროექტის მენეჯერი'
+    },
+    {
+      name: 'ნინი წიკლაური',
+      img: '',
+      role: 'პროექტის მენეჯერი'
+    },
+    {
+      name: 'ნინი წიკლაური',
+      img: '',
+      role: 'UI/UX დიზაინერი'
+    },
+    {
+      name: 'ნინი წიკლაური',
+      img: '',
+      role: 'პროექტის მენეჯერი'
+    },
+    {
+      name: 'ნინი წიკლაური',
+      img: '',
+      role: 'პროექტის მენეჯერი'
+    },
+    {
+      name: 'ნინი წიკლაური',
+      img: '',
+      role: 'პროექტის მენეჯერი'
+    }
+  ],
+  duration: '4 თვე',
+  link: '',
+  usedTools: ['', '', ''],
+}
