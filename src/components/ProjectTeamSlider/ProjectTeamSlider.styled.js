798f8b4a106b9ba1051997f9f2c6ef65e86e357c
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-28 16:20:50 +0400

    add starting test styles for slider

---
+++ src/components/ProjectTeamSlider/ProjectTeamSlider.styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+export const SProjectTeamSlider = styled.div`
+  width: 100%;
+`
+export const SMemberDiv = styled.div`
+  width: 14.1875rem;
+  height: 14.1875rem;
+  background-color: red; // test
+  border: 1px solid blue; // test
+  display: flex !important; 
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+`
+export const SMemberImg = styled.img`
+  width: 6rem;
+  height: 6rem;
+  border-radius: 100%;
+  border: .625rem;
+  object-fit: cover;
+`
