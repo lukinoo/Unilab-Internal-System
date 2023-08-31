864da7f45b539df15719d0ab06b6d671767bbf43
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-31 16:07:47 +0400

    display components; add footer usedToolsDiv

--- src/views/ProjectPage/ProjectPage.jsx
+++ src/views/ProjectPage/ProjectPage.jsx
@@ -7,10 +7,17 @@ import {
   SLargeText,
   SProjectRole,
   SSliderDiv,
+  SProjectDuration,
+  SProjectPageFooter,
+  SWrapper,
+  SProjectLink,
+  SUsedToolsDiv,
+  STitle,
 } from "./ProjectPage.styled";
 import { ProjectSlider } from "../../components/ProjectSlider";
 import { TestProjectData } from "./TestData";
 import { ProjectTeamSlider } from "../../components/ProjectTeamSlider/ProjectTeamSlider";
+import { Link } from "react-router-dom";
 
 // user = person whose porfolio link was used to load the page
 export const ProjectPage = () => {
@@ -30,37 +37,55 @@ export const ProjectPage = () => {
   return (
     <SProjectPageMainDiv>
       <FourthHeader />
-      <h2>საფინალო პროექტი</h2>
+      <STitle>საფინალო პროექტი</STitle>
       <ProjectSlider
         onChange={handleSliderChange}
         data={TestProjectData.images}
         imgHeight={"37.5rem"}
       />
-      <SInfoDiv>
-        <SLargeText>
-          პროექტის დასახელება: <span>{TestProjectData.name}</span>
-        </SLargeText>
-        <p>პროექტის აღწერა:</p>
-        <SDescriptionDiv>
-          <p>{TestProjectData.description}</p>
-        </SDescriptionDiv>
-      </SInfoDiv>
-      <SSliderDiv>
-        <p>პროექტზე მუშაობდნენ:</p>
-        <ProjectTeamSlider />
-      </SSliderDiv>
-      <SInfoDiv>
-        <SProjectRole>
-          ჩემი როლი: <span>{role}</span>
-        </SProjectRole>
-        <SDescriptionDiv width={"75%"}>
-          <p>{roleDescription}</p>
-        </SDescriptionDiv>
-      </SInfoDiv>
-      <SLargeText>
-        პროექტის ხანგრძლივობა: <span>{TestProjectData.duration}</span>
-      </SLargeText>
-      {/* Footer */}
+      <SWrapper>
+        <SInfoDiv>
+          <SLargeText>
+            პროექტის დასახელება: <span>{TestProjectData.name}</span>
+          </SLargeText>
+          <p>პროექტის აღწერა:</p>
+          <SDescriptionDiv>
+            <p>{TestProjectData.description}</p>
+          </SDescriptionDiv>
+        </SInfoDiv>
+
+        <SSliderDiv>
+          <p>პროექტზე მუშაობდნენ:</p>
+          <ProjectTeamSlider />
+        </SSliderDiv>
+
+        <SInfoDiv>
+          <SProjectRole>
+            ჩემი როლი: <span>{role}</span>
+          </SProjectRole>
+          <SDescriptionDiv width={"75%"}>
+            <p>{roleDescription}</p>
+          </SDescriptionDiv>
+        </SInfoDiv>
+
+        <SProjectDuration>
+          პროექტის ხანგრძლივობა: <span>{TestProjectData.duration}</span>
+        </SProjectDuration>
+
+        <SProjectPageFooter>
+          <SProjectLink>
+            <Link to={TestProjectData.link}>
+              <span>პროექტის ლინკი</span>
+            </Link>
+          </SProjectLink>
+          <SUsedToolsDiv>
+            <p>გამოყენებული ხელსაწყოები</p>
+            <div>
+                {/* Used tools:  */}
+            </div>
+          </SUsedToolsDiv>
+        </SProjectPageFooter>
+      </SWrapper>
     </SProjectPageMainDiv>
   );
 };
