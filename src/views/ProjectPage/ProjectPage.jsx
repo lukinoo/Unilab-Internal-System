6fde337cd5b5be959394cddc369f2f560943fe8f
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-31 14:06:49 +0400

    display components; format code

--- src/views/ProjectPage/ProjectPage.jsx
+++ src/views/ProjectPage/ProjectPage.jsx
@@ -1,21 +1,29 @@
-import { useState } from "react"
-import { FourthHeader } from "../../components/Headers/FourthHeader"
-import { SProjectPageMainDiv } from "./ProjectPage.styled"
-import { ProjectSlider } from "../../components/ProjectSlider"
-import { TestProjectData } from "./TestData"
+import { useState } from "react";
+import { FourthHeader } from "../../components/Headers/FourthHeader";
+import {
+  SProjectPageMainDiv,
+  SInfoDiv,
+  SDescriptionDiv,
+  SLargeText,
+  SProjectRole,
+  SSliderDiv,
+} from "./ProjectPage.styled";
+import { ProjectSlider } from "../../components/ProjectSlider";
+import { TestProjectData } from "./TestData";
+import { ProjectTeamSlider } from "../../components/ProjectTeamSlider/ProjectTeamSlider";
 
 // user = person whose porfolio link was used to load the page
 export const ProjectPage = () => {
-  const user = 'ნინი წიკლაური'; // temporary string; **receive the user as a prop**
+  const user = "ნინი წიკლაური"; // temporary string; **receive the user as a prop**
   const [sliderIndex, setSliderIndex] = useState(0);
 
   const handleSliderChange = (i) => {
     setSliderIndex(i);
   };
-  const findUserObj = () =>{
-    const userObj = TestProjectData.team.filter(obj=>obj.name == user)[0];
+  const findUserObj = () => {
+    const userObj = TestProjectData.team.filter((obj) => obj.name == user)[0];
     return userObj;
-  }
+  };
   const role = findUserObj(user).role;
   const roleDescription = findUserObj(user).roleDescription;
 
@@ -23,23 +31,36 @@ export const ProjectPage = () => {
     <SProjectPageMainDiv>
       <FourthHeader />
       <h2>საფინალო პროექტი</h2>
-      <ProjectSlider onChange={handleSliderChange} data={TestProjectData.images} imgHeight={'37.5rem'}/>
-      {/* Project info */}
-      <div>
-        <p>პროექტის დასახელება: {TestProjectData.name}</p>
+      <ProjectSlider
+        onChange={handleSliderChange}
+        data={TestProjectData.images}
+        imgHeight={"37.5rem"}
+      />
+      <SInfoDiv>
+        <SLargeText>
+          პროექტის დასახელება: <span>{TestProjectData.name}</span>
+        </SLargeText>
         <p>პროექტის აღწერა:</p>
-        <p>{TestProjectData.description}</p>
-      </div>
-      {/* Project info */}
-      {/* img slider */}
-      {/* My role */}
-      <div>
-        <p>ჩემი როლი: {role}</p>
-        <p>{roleDescription}</p>
-      </div>
-      <p>პროექტის ხანგრძლივობა: {TestProjectData.duration}</p>
-      {/* Project link */}
-      {/* Used tools */}
+        <SDescriptionDiv>
+          <p>{TestProjectData.description}</p>
+        </SDescriptionDiv>
+      </SInfoDiv>
+      <SSliderDiv>
+        <p>პროექტზე მუშაობდნენ:</p>
+        <ProjectTeamSlider />
+      </SSliderDiv>
+      <SInfoDiv>
+        <SProjectRole>
+          ჩემი როლი: <span>{role}</span>
+        </SProjectRole>
+        <SDescriptionDiv width={"75%"}>
+          <p>{roleDescription}</p>
+        </SDescriptionDiv>
+      </SInfoDiv>
+      <SLargeText>
+        პროექტის ხანგრძლივობა: <span>{TestProjectData.duration}</span>
+      </SLargeText>
+      {/* Footer */}
     </SProjectPageMainDiv>
   );
-}
+};
