d4b9f083cc1e93202529245eaa4b9cd0ba398c66
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-31 16:18:14 +0400

    add object to display images of used tools

---
+++ src/views/ProjectPage/data/tools.js
@@ -0,0 +1,9 @@
+import adobeIllustratorSvg from "/assets/svg/adobeIllustratorIcon.svg";
+import adobeXdSvg from "/assets/svg/adobeXdIcon.svg";
+import figmaSvg from "/assets/svg/figmaIcon.svg";
+
+export const tools = {
+  'adobe illustrator': adobeIllustratorSvg,
+  'adobe xd': adobeXdSvg,
+  'figma': figmaSvg,
+}
