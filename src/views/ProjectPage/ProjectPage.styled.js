c0d87dd02356f3462321a61366d1931f9382063d
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-08-31 12:37:05 +0400

    add width prop to SDescriptionDiv

--- src/views/ProjectPage/ProjectPage.styled.js
+++ src/views/ProjectPage/ProjectPage.styled.js
@@ -18,6 +18,7 @@ export const SInfoDiv = styled.div`
 `
 
 export const SDescriptionDiv = styled.div`
+  width: ${({width})=>width};
   padding: 1rem 2rem 2.5rem;
   background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='16' ry='16' stroke='%233F48CC' stroke-width='4' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
   border-radius: 1rem;
