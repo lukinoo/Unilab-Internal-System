909ca780ba4c0a28e43870fdecaf6566c30d33e8
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-04 13:08:15 +0400

    add custom border to uploader component

--- src/views/UploadProject/components/Uploader.styled.js
+++ src/views/UploadProject/components/Uploader.styled.js
@@ -45,7 +45,8 @@ export const SUploaderWrapper = styled.div`
   padding: 3.25rem 3.75rem;
   gap: 4.563rem;
   min-height: 13rem;
-  border: 1px dashed white;
+  background-image: url("data:image/svg+xml,%3csvg width='100%25' height='100%25' xmlns='http://www.w3.org/2000/svg'%3e%3crect width='100%25' height='100%25' fill='none' rx='10' ry='10' stroke='%23ffffff' stroke-width='3' stroke-dasharray='6%2c 14' stroke-dashoffset='0' stroke-linecap='square'/%3e%3c/svg%3e");
+  border-radius: 0.625rem;
 `;
 
 export const SOverlay = styled.div`
