1e360963a958eb8673a0c974a4a1b0e63e769154
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-07 18:06:23 +0400

    add styled customInput component

---
+++ src/components/Inputs/RadioDropdown/RadioDropdown.styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { SInput } from "../Input/Input.styled";
+
+export const SCustomInput = styled(SInput)`
+  height: 1.875rem;
+  font-size: 0.75rem;
+  border: 0.0625rem solid #2984CE;
+  border-radius: 0.375rem;
+  padding: 0.375rem 0.625rem;
+  color: #7D7D7D;
+  font-family: 'firaGO';
+`
