436f0e7600772d16010e354d877104b2bcfb99f4
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-07 17:19:50 +0400

    add RadioDropdown component with it's styles as a copy of dropdown

---
+++ src/components/Inputs/RadioDropdown/RadioDropdown.jsx
@@ -0,0 +1,83 @@
+import React, { useRef } from "react";
+import { Input } from "../Input";
+import {
+  SArrowButton,
+  SDropdownButton,
+  SDropdownItem,
+  SDropdownList,
+  SDropdownWrapper,
+} from "../Dropdown/Dropdown.styled";
+import { DropArrow } from "../../DropArrow";
+import { dropdownVariants } from "./RadioDropdown.variants";
+import { getLongestString } from "../../../utils/dropdown";
+import { useAutoClose } from "../../../hooks/useAutoClose";
+import { AnimatePresence } from "framer-motion";
+import { useController } from "react-hook-form";
+
+export const RadioDropdown = (props) => {
+  const { name, gridArea, items, control } = props;
+
+  const {
+    field: { value, onChange, onBlur },
+  } = useController({ name, control });
+
+  const dropdownRef = useRef(null);
+  const [isOpen, setIsOpen] = useAutoClose(dropdownRef, false, onBlur);
+
+  const longestItem = items && getLongestString(Object.values(items));
+
+  const toggleOpen = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleSelect = (id) => {
+    toggleOpen();
+    onChange(id);
+  };
+
+  return (
+    <SDropdownWrapper gridArea={gridArea} ref={dropdownRef}>
+      <Input
+        {...props}
+        value={items && items[value]}
+        readOnly
+        type={"text"}
+        onClick={toggleOpen}
+        RightComponent={
+          <SArrowButton type="button" title="toggle dropdown">
+            <DropArrow isOpen={isOpen} />
+          </SArrowButton>
+        }
+      />
+      <AnimatePresence>
+        {isOpen && (
+          <SDropdownList
+            variants={dropdownVariants}
+            initial={"hidden"}
+            animate={"visible"}
+            exit={"hidden"}
+            transition={{ duration: 0.4, type: "spring" }}
+          >
+            {!items ? (
+              <span>--- no items ---</span>
+            ) : (
+              Object.entries(items).map(([id, value]) => {
+                return (
+                  <SDropdownItem key={id}>
+                    <SDropdownButton
+                      type="button"
+                      onClick={() => handleSelect(id)}
+                      data-longestitem={longestItem}
+                    >
+                      {value}
+                    </SDropdownButton>
+                  </SDropdownItem>
+                );
+              })
+            )}
+          </SDropdownList>
+        )}
+      </AnimatePresence>
+    </SDropdownWrapper>
+  );
+};
---
+++ src/components/Inputs/RadioDropdown/index.js
@@ -0,0 +1,1 @@
+export { RadioDropdown } from "./RadioDropdown";
