2e980ee63c075d6a9af756dab6a4b12028a93750
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-12 22:57:20 +0400

    add basic overlay logic

--- src/components/Inputs/LinkUploader/LinkUploader.styled.js
+++ src/components/Inputs/LinkUploader/LinkUploader.styled.js
@@ -1,6 +1,15 @@
 import styled from "styled-components";
 
+
+export const SOverlay = styled.div`
+  position: absolute;
+  display: none;
+  background-color: blue;
+  width: inherit;
+  height: inherit;
+`
 export const SLinkUploader = styled.div`
+  position: relative;
   width: 18.4375rem;
   height: 6.3125rem;
   border-radius: .625rem;
@@ -8,6 +17,11 @@ export const SLinkUploader = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  :hover{
+    ${SOverlay}{
+      display: block;
+    }
+  }  
 `
 export const SLInkIcon = styled.img`
   margin-left: 1.5rem;
