919f0dadade1065c4f077eaf92f52a1c819a1426
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-13 00:10:46 +0400

    format code

--- src/components/Inputs/LinkUploader/LinkUploader.styled.js
+++ src/components/Inputs/LinkUploader/LinkUploader.styled.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 
-
 export const SOverlay = styled.div`
   position: absolute;
   display: none;
@@ -9,42 +8,43 @@ export const SOverlay = styled.div`
   border-radius: inherit;
   align-items: center;
   justify-content: space-around;
-`
+`;
 export const SLinkUploader = styled.div`
   position: relative;
   width: 18.4375rem;
   height: 6.3125rem;
-  border-radius: .625rem;
-  border: .125rem solid #2984CE;
+  border-radius: 0.625rem;
+  border: 0.125rem solid #2984ce;
   display: flex;
   justify-content: center;
   align-items: center;
-  :hover{
-    ${SOverlay}{
+  :hover {
+    ${SOverlay} {
       display: flex;
-      background: rgba(255, 255, 255, 0.70);
+      background: rgba(255, 255, 255, 0.7);
     }
-  }  
-`
+  }
+`;
 const SIcon = styled.img`
-`
+  cursor: pointer;
+`;
 
 export const SLInkIcon = styled(SIcon)`
   margin-left: 1.5rem;
-`
+`;
 export const SEditIcon = styled(SIcon)`
-  filter: invert(100%)
-`
-export const SDeleteIcon = styled(SEditIcon)`
-`
+  filter: invert(100%);
+`;
+export const SDeleteIcon = styled(SEditIcon)``;
 export const SIconWrapper = styled.div`
   border-radius: 0.375rem;
   padding: 0.5rem 1.5rem;
   background-color: #ffffff;
-`
+  cursor: pointer;
+`;
 export const SIconsDiv = styled.div`
   width: 60%;
-  display:flex;
+  display: flex;
   align-items: center;
   justify-content: space-between;
-`
+`;
