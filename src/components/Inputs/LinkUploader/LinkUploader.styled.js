1a53e08812ecc0a8da1d66a7dbfc9ed4f4ba14a5
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-12 22:47:29 +0400

    add basic styles for linkUploader

---
+++ src/components/Inputs/LinkUploader/LinkUploader.styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SLinkUploader = styled.div`
+  width: 18.4375rem;
+  height: 6.3125rem;
+  border-radius: .625rem;
+  border: .125rem solid #2984CE;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+export const SLInkIcon = styled.img`
+  margin-left: 1.5rem;
+`
