b1fc0972cbc5d34f0bc8af9f93ecce89f7e47bc6
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-13 21:27:30 +0400

    remove unused styled component

--- src/views/UploadProjectAdmin/UploadProjectAdmin.styled.js
+++ src/views/UploadProjectAdmin/UploadProjectAdmin.styled.js
@@ -48,14 +48,6 @@ export const SPositionWrapper = styled.div`
   display: flex;
   flex-direction: column;
 `;
-
-export const SButtonWrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 3.563rem;
-  margin: 5.5rem auto 0 auto;
-`;
 export const SAdditionalDataDiv = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
