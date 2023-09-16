f653731e7ca617e258fff71b7096a214aa6e614d
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-16 10:17:58 +0400

    add optional label to textarea component; format code

--- src/components/Inputs/Textarea/Textarea.jsx
+++ src/components/Inputs/Textarea/Textarea.jsx
@@ -1,22 +1,38 @@
-import { STextarea } from "./Textarea.styled";
+import { SLabel } from "../Input/Input.styled";
+import { STextarea, STextareaDiv } from "./Textarea.styled";
 import { useController } from "react-hook-form";
 
-export const Textarea = ({ name, control, placeholder, bgColor, color, resize, width, height}) => {
+export const Textarea = ({
+  name,
+  control,
+  placeholder,
+  bgColor,
+  color,
+  resize,
+  width,
+  height,
+  label,
+  marginTop,
+}) => {
   const {
     field: { value, onChange },
   } = useController({ name, control });
 
   return (
-    <STextarea
-      onChange={onChange}
-      placeholder={placeholder}
-      control={control}
-      name={name}
-      bgColor={bgColor}
-      color={color}
-      resize={resize}
-      width={width}
-      height={height}
-    />
+    <STextareaDiv>
+      {label && <SLabel>{label}</SLabel>}
+      <STextarea
+        onChange={onChange}
+        placeholder={placeholder}
+        control={control}
+        name={name}
+        bgColor={bgColor}
+        color={color}
+        resize={resize}
+        width={width}
+        height={height}
+        marginTop={marginTop}
+      />
+    </STextareaDiv>
   );
 };
