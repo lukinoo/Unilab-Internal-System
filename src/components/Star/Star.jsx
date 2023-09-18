1ceb970e21bdddd4a9ddae66a41799972f4b92c4
Luca Gabadadze <gabadadzeluca@gmail.com>
2023-09-18 11:28:46 +0400

    add index file for Star; Update exports

--- src/components/Star/Star.jsx
+++ src/components/Star/Star.jsx
@@ -5,7 +5,7 @@ import starRecoverySvg from "/assets/svg/starRecoveryBottom.svg";
 import starLeftTopSvg from "/assets/svg/RStarLeftTop.svg";
 import starLeftBottomSvg from "/assets/svg/RStarLeftBottom.svg";
 
-const obj = {
+const STARS = {
   1: starTopSvg,
   2: starBottomSvg,
   3: starRecoverySvg,
@@ -14,7 +14,7 @@ const obj = {
 };
 
 export const Star = ({ number, top, left, width, height }) => {
-  return <SStar src={obj[number]} top={top} left={left} />;
+  return <SStar src={STARS[number]} top={top} left={left} />;
 };
 
 const SStar = styled.img`
